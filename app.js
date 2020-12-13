a6bf818650cf7ff5d2064abda2cde703a30b97a2
Anurag Kumar <anuragsingh4512@gmail.com>
2020-12-13 19:50:04 +0530

    Browser events added

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var txtInput= document.querySelector("#txt-input");
 console.log(txtInput);
+//console.log("Input"+txtInput.value);
+txtInput.onclick = function clickHandler(){
+    console.log("Input "+txtInput.value);
+}
